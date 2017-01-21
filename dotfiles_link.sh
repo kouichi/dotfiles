cb8c76b187fc3ecff2a4b3cd469e341923957fe8
Kouichi OHTA <kouichi.ohta@gmail.com>
2017-01-21 19:36:18 +0900

    remove unsed dotfiles

--- dotfiles_link.sh
+++ dotfiles_link.sh
@@ -2,12 +2,6 @@
 
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.zlogin ~/.zlogin
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
 
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/.hgrc ~/.hgrc
-ln -sf ~/dotfiles/.hgignore_global ~/.hgignore_global
